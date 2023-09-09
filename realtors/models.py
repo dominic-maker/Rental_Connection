6f16f1981749da4e969f89dc125a38bb3e43b538
Dominic Tech <70665455+dominic-maker@users.noreply.github.com>
2023-09-09 19:11:17 +0300

    Add files via upload

---
+++ realtors/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Realtor(models.Model):
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to="photos/%Y%m/%d/", blank=True)
+    description = models.TextField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=255)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
